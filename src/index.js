9a487bfe5cd08c8264a60301d4cf9e2cc745c03b
wildpow <element1028@gmail.com>
2019-02-19 16:10:26 -0800

    added normalize and fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createGlobalStyle } from "styled-components";
+import styledNormalize from "styled-normalize";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const GlobalStyle = createGlobalStyle`
+  ${styledNormalize}
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans|Roboto:700');
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById("root")
+);
